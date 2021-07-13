26b6a6e579e6fdcd69012b0e80a477652de815ec
Artiom Ciobanu <aciobanumain@gmail.com>
2021-07-13 15:20:54 +0300

    [TSO-4] AccountController finished

--- Source/SingleSignOn/SingleSignOn/Startup.cs
+++ Source/SingleSignOn/SingleSignOn/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,32 +12,78 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using WebApiBaseLibrary.Authorization.Configurators;
+using WebApiBaseLibrary.Authorization.Constants;
+using WebApiBaseLibrary.Authorization.Extensions;
+using WebApiBaseLibrary.Authorization.Models;
 
 namespace SingleSignOn
 {
     public class Startup
     {
+        private IServiceProvider _serviceProvider;
+
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingletonJwtConfiguration(Configuration);
+
+            services.AddSingleton<IJwtConfigurator, JwtConfigurator>();
+
+            services.AddConfiguredJwtBearer(_serviceProvider);
+            services.AddAuthorization();
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
+            services.AddSwaggerGen(options =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SingleSignOn", Version = "v1" });
+                var securityScheme = new OpenApiSecurityScheme
+                {
+                    Description = "Json Web Token for authorization. Write: 'Bearer {your token}'",
+                    Name = HeaderNames.Authorization,
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = Schemes.Bearer
+                };
+                options.AddSecurityDefinition(securityScheme.Scheme, securityScheme);
+
+                var requirement = new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = securityScheme.Scheme
+                            },
+                            Scheme = Schemes.OAuth,
+                            Name = securityScheme.Scheme,
+                            In = securityScheme.In
+                        },
+                        new List<string>()
+                    }
+                };
+
+                options.AddSecurityRequirement(requirement);
+
+                options.SwaggerDoc("v1", new OpenApiInfo {Title = "SingleSignOn", Version = "v1"});
             });
+
+            services.AddMediatR(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            _serviceProvider = app.ApplicationServices;
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -50,10 +97,7 @@ namespace SingleSignOn
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
