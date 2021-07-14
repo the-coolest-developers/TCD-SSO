785d9020f843a92fcb07d1930d6e5ac7864d941e
Artiom Ciobanu <aciobanumain@gmail.com>
2021-07-14 23:59:12 +0300

    [TSO-9] Added AccountContext, PostgreSQL provider, Account Entity

---
+++ Source/SingleSignOn/SingleSignOn.DataAccess/Entities/Account.cs
@@ -0,0 +1,11 @@
+﻿using WebApiBaseLibrary.DataAccess.Entities;
+
+namespace SingleSignOn.DataAccess.Entities
+{
+    public class Account : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
